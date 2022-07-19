973dcba3b5e802ae73d77efae4cdb90382778241
madiganz <zacharywmadigan@gmail.com>
2022-07-18 23:07:17 -0700

    Add coinbase wallet support

--- src/providerOptions.js
+++ src/providerOptions.js
@@ -1,14 +1,14 @@
 import WalletConnect from "@walletconnect/web3-provider";
-// import CoinbaseWalletSDK from "@coinbase/wallet-sdk";
+import CoinbaseWalletSDK from "@coinbase/wallet-sdk";
 
 export const providerOptions = {
-//   walletlink: {
-//     package: CoinbaseWalletSDK, // Required
-//     options: {
-//       appName: "Web 3 Modal Demo", // Required
-//       infuraId: process.env.INFURA_KEY // Required unless you provide a JSON RPC url; see `rpc` below
-//     }
-//   },
+  walletlink: {
+    package: CoinbaseWalletSDK, // Required
+    options: {
+      appName: "Refractly", // Required
+      infuraId: process.env.REACT_APP_INFURA_KEY // Required unless you provide a JSON RPC url; see `rpc` below
+    }
+  },
   walletconnect: {
     package: WalletConnect, // required
     options: {
