eae6ca032fc37e7eb33f4e45d8906d53c431f150
madiganz <zacharywmadigan@gmail.com>
2022-07-20 13:32:27 -0700

    Display percentages for reward tooltip

--- src/RewardPanel.js
+++ src/RewardPanel.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Grid from '@mui/material/Grid';
 import DisplayBox from './DisplayBox';
 import Tooltip from '@mui/material/Tooltip';
+import { formatAsPercent } from './utils';
 
 const RewardPanel = ({
     rewardData
@@ -21,11 +22,11 @@ const RewardPanel = ({
 
                 return (
                     <Grid item key={reward[0]}>
-                        <Tooltip enterNextDelay={1000} title={
+                        <Tooltip enterDelay={500} enterNextDelay={500} title={
                             <React.Fragment>
-                            <b>Pool Staking: </b>{Number(data.poolEarned / diviser).toFixed(8)}<br></br>
-                            <b>penDYST Staking: </b>{Number(data.penDystEarned / diviser).toFixed(8)}<br></br>
-                            <b>vIPEN Locking: </b>{Number(data.vIPenEarned / diviser).toFixed(8)}
+                            <b>Pool Staking: </b>{formatAsPercent((data.poolEarned / diviser) / rewards)}<br></br>
+                            <b>penDYST Staking: </b>{formatAsPercent((data.penDystEarned / diviser) / rewards)}<br></br>
+                            <b>vIPEN Locking: </b>{formatAsPercent((data.vIPenEarned / diviser) / rewards)}
                             </React.Fragment>
                         }>
                             <DisplayBox
--- src/utils.js
+++ src/utils.js
@@ -1,14 +1,22 @@
 export const truncateAddress = (address) => {
-    if (!address) return "No Account";
-    const match = address.match(
-      /^(0x[a-zA-Z0-9]{4})[a-zA-Z0-9]+([a-zA-Z0-9]{4})$/
-    );
-    if (!match) return address;
-    return `${match[1]}…${match[2]}`;
-  };
+  if (!address) return "No Account";
+  const match = address.match(
+    /^(0x[a-zA-Z0-9]{4})[a-zA-Z0-9]+([a-zA-Z0-9]{4})$/
+  );
+  if (!match) return address;
+  return `${match[1]}…${match[2]}`;
+};
   
-  export const toHex = (num) => {
-    const val = Number(num);
-    return "0x" + val.toString(16);
-  };
+export const toHex = (num) => {
+  const val = Number(num);
+  return "0x" + val.toString(16);
+};
+
+export const formatAsPercent = (num) => {
+  return new Intl.NumberFormat('default', {
+    style: 'percent',
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2,
+  }).format(num);
+}
   
